c7115caa09959fbdbc88377d8753fb4a1711f5bd
Andrii <111135827+mangystx@users.noreply.github.com>
2024-02-11 17:19:30 +0200

    create extensions module for Puppeteersharp and Playwright (#4)

---
+++ src/NBomber.WebBrowser/PlaywrightExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+using NBomber.CSharp;
+
+namespace NBomber.WebBrowser.Playwright;
+
+public static class PlaywrightExtension
+{
+    public static async Task<Contracts.Response<IResponse>> ToNBomberResponse(this IResponse response)
+    {
+        var respBody = await response.BodyAsync();
+        var respHeaders = await response.AllHeadersAsync();
+        var reqHeaders = await response.Request.AllHeadersAsync();
+
+        var sizeBytes = respBody.Length + respHeaders.Sum(kv => kv.Value.Length + kv.Key.Length) 
+                                        + (response.Request.PostDataBuffer?.Length ?? 0) 
+                                        + reqHeaders.Sum(kv => kv.Value.Length + kv.Key.Length);
+
+        return Response.Ok(payload: response, statusCode: response.Status.ToString(), sizeBytes: sizeBytes);
+    }
+}
